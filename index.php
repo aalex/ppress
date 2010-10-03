bfcab7609da579ad5a6893cadc1051e9bdb276a6
Alexandre Quessy <alexandre@quessy.net>
2010-10-03 08:43:09 -0400

    initial import

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Picture Press</title>
+    <script src="js/jquery-1.4.2.min.js" type="text/javascript" /></script>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script src="js/picturepress.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        /* pass */
+    </script>
+</head>
+<body>
+<div id="container">
+    <a href="http://alexandre.quessy.net/">Alexandre Quessy</a>
+</div>
+</body>
+</html>
+
---
+++ js/picturepress.js
@@ -0,0 +1,14 @@
+/**
+ * Javascript script.
+ */
+$(document).ready(function()
+{
+    $("a").click(function(event)
+    {
+        alert("As you can see, the link no longer took you to jquery.com");
+        event.preventDefault();
+        $(this).hide("slow");
+    });
+    $("a").addClass("test");
+});
+
