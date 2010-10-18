a069049b6e5ed93fa546b0f753b04c255d274512
Alexandre Quessy <alexandre@quessy.net>
2010-10-18 03:35:34 -0400

    favicon works in firefox

--- tpl/page.tpl.php
+++ tpl/page.tpl.php
@@ -8,6 +8,8 @@
     <meta name="robots" content="index, follow" />
     <!-- FIXME: this icon does not exist -->
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    </link>
     <link href="css/single_column.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
