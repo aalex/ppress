9f95fcc3cef98fb54e008973eed5366dcbcc4090
Alexandre Quessy <alexandre@quessy.net>
2010-10-18 03:30:15 -0400

    default verbose is false

--- php/config.example.php
+++ php/config.example.php
@@ -7,7 +7,7 @@ define("DB_USER", "picturepress");
 define("DB_DATABASE", "picturepress");
 define("DB_PASSWORD", "picturepress");
 define("DB_HOST", "localhost");
-define("PP_VERBOSE", TRUE);
+define("PP_VERBOSE", FALSE);
 define("FLICKR_APIKEY", "");
 define("FLICKR_CC_ONLY", TRUE);
 ?>
