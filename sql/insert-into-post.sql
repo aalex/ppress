4716408f07fbc7c335a69c8e324eb8157f6c975f
Alexandre Quessy <alexandre@quessy.net>
2010-11-03 21:22:50 -0400

    adding some sql inserts and dumps

---
+++ sql/insert-into-post.sql
@@ -0,0 +1,11 @@
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,1,'[转载]谁为亡国灭种的楼市巨泡敲响丧钟？','2010-11-04 00:44:11','http://blog.sina.com.cn/s/blog_60eca5730100m97x.html','2010-11-04 00:44:11','2010-11-04 00:44:11'),(2,1,'[转载]谁为亡国灭种的楼市巨泡敲响丧钟？','2010-11-04 00:46:04','http://blog.sina.com.cn/s/blog_60eca5730100m97x.html','2010-11-04 00:46:04','2010-11-04 00:46:04'),(3,1,'[转载]谁为亡国灭种的楼市巨泡敲响丧钟？','2010-11-04 00:46:25','http://blog.sina.com.cn/s/blog_60eca5730100m97x.html','2010-11-04 00:46:25','2010-11-04 00:46:25'),(4,1,'[转载]谁为亡国灭种的楼市巨泡敲响丧钟？','2010-11-04 00:47:49','http://blog.sina.com.cn/s/blog_60eca5730100m97x.html','2010-11-04 00:47:49','2010-11-04 00:47:49');
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
